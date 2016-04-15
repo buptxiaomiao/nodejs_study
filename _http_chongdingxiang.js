3b12c0ba05b1d23325086d59d545996eb47a81fc
buptxiaomiao <buptwjh@outlook.com>
2016-04-15 16:38:25 +0800

    nodejs的http服务器

---
+++ _http_chongdingxiang.js
@@ -0,0 +1,10 @@
+
+var http = require("http");
+http.createServer(function(req,res){
+	res.writeHead(301,{
+		'Location':'http://baidu.com'
+	});
+	res.end();
+}).listen(3000,"127.0.0.1");
+
+console.log("Server running at http://127.0.01:3000");
